a3c53e9b26027af110db0acbc89dd8fc66906d25
Em Caringal <91007625+ircaringal@users.noreply.github.com>
2024-04-06 14:46:23 +0800

    permutohedron

--- permutohedron.py
+++ permutohedron.py
@@ -1,49 +1,25 @@
-import plotly.express as px
-import numpy as np
-import itertools
-import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
 
-order = int(input())
+def check_swap(str1, str2):
+      for i in range(len(str1) - 1):
+            if str1[i] == str2[i+1] and str1[i+1] == str2[i]:
+                  return True
+      return False
+
 inp = []
 for i in range(int(input())):
-      inp.append(list(int(i) for i in input()))
-permuted_items = np.array(inp)
+      inp.append(input())
+      
+print(inp)
 
-#Project the points onto 3D space--
-A = np.array([[np.sqrt(2)/2, -np.sqrt(2)/2, 0, 0],\
-     [np.sqrt(6)/6, np.sqrt(6)/6, -np.sqrt(2/3), 0],\
-     [np.sqrt(12)/12, np.sqrt(12)/12, np.sqrt(12)/12, -np.sqrt(3)/2]])
+G = nx.Graph()
+G.add_nodes_from(inp)
 
-permuted_items = np.einsum('ik,ak->ai',A,permuted_items)
-#----------------------------------
+for i in range(len(inp)-1):
+      for j in range(i+1,len(inp)):
+            if check_swap(inp[i], inp[j]):
+                  G.add_edge(inp[i], inp[j])
 
-xyzs = []
-colors = []
-
-for i, point in enumerate(permuted_items):
-
-      d = np.linalg.norm(permuted_items-point[np.newaxis,:],axis=1)
-
-      #Inspired by https://stackoverflow.com/questions/31352486/can-numpy-argsort-handle-ties 
-      js = (abs(d - d[d.argsort()[1]])<1e-3).nonzero()[0]
-
-      for j in js:
-          xyzs.extend([point,permuted_items[j]])
-
-          # Get unique string as color to group above line while plotting
-          c = str(point) + str(permuted_items[j])
-          colors.extend([c, c])
-
-
-lines = np.array(xyzs)
-x, y, z = lines.T
-
-plotting_data = pd.DataFrame({
-      "X": x,
-      "Y": y,
-      "Z": z,
-      "color": colors,
-})
-
-fig = px.line_3d(plotting_data, x='X', y='Y', z='Z', color="color")
-fig.show()
+nx.draw_kamada_kawai(G, with_labels=True)
+plt.show()
