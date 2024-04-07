322de247dbf99e33a1bca920da37b2a0e983d8d0
Em Caringal <91007625+ircaringal@users.noreply.github.com>
2024-04-07 12:43:17 +0800

    working permutohedron

--- permutohedron.py
+++ permutohedron.py
@@ -4,8 +4,10 @@ import matplotlib.pyplot as plt
 def check_swap(str1, str2):
       for i in range(len(str1) - 1):
             if str1[i] == str2[i+1] and str1[i+1] == str2[i]:
-                  return True
-      return False
+                  if str1[i+2:] != str2[i+2:]:
+                        return None
+                  return f"{str1[i]}, {str2[i]}"
+      return None
 
 inp = []
 for i in range(int(input())):
@@ -18,8 +20,11 @@ G.add_nodes_from(inp)
 
 for i in range(len(inp)-1):
       for j in range(i+1,len(inp)):
-            if check_swap(inp[i], inp[j]):
-                  G.add_edge(inp[i], inp[j])
+            adjacent = check_swap(inp[i], inp[j])
+            if adjacent:
+                  G.add_edge(inp[i], inp[j], label=adjacent)
 
-nx.draw_kamada_kawai(G, with_labels=True)
+pos = nx.kamada_kawai_layout(G)
+nx.draw(G, pos, with_labels=True)
+nx.draw_networkx_edge_labels(G, pos, edge_labels=nx.get_edge_attributes(G,'label'))
 plt.show()
