c4bb842f741c78cd877c76c521b7f9e03f69917a
Kharis Arielle Ann Hipe <91011467+annhipe@users.noreply.github.com>
2024-06-08 00:33:07 +0800

    Algorithm 2 updates

--- Algorithm/1updated/algo2.py
+++ Algorithm/1updated/algo2.py
@@ -3,176 +3,82 @@ import networkx as nx
 import pylab as p
 from collections import defaultdict, OrderedDict
 from poset_utils import VERIFY, get_linear_extensions, binaryRelation, binaryToCover, superCover, isPoset
+from algo1 import generatePoset
+from permutohedron import check_swap
 
-def Poset(upsilon):
-    Pset = []
-    
-    # Generate Transposition Graph
+def maximalPoset(upsilon, P_A, A):
     G = nx.Graph()
-    l = len(upsilon)
-    nodes = upsilon
-    Edges = dict([])
-    #print(upsilon)
-    for u in upsilon:
-        Edges[u] = []
-    
-    
+    N = len(upsilon)
+
+    # Add an edge between each node that has an adjacent swap
+    for i in range(N):
+        G.add_node(upsilon[i])
+        for j in range(i + 1, N):
+            adjacent = check_swap(upsilon[i], upsilon[j])
+            if adjacent:
+                G.add_edge(upsilon[i], upsilon[j], label=adjacent, color='k')
+
+    # Find linear extensions
+    Y_cov = get_linear_extensions(P_A)
+    print("Y_cov", Y_cov)
+    Y_uncov = [u for u in upsilon if u not in Y_cov]
+    print("Y_uncov", Y_uncov)
+
+    # Create a dictionary of neighbor nodes for each node
+    Neighbors = {n: list(G.neighbors(n)) for n in G.nodes()}
+
+    #print("Neighbors", Neighbors)
+
     """
-    The code below generates pairs from after the starting number, and 
-    finds linear orders with its mirror and pair that has its mirror
-    e.g.
-    pairs: ['34']
-    Edges {'1234': [[(3, 4), '1243']], '1243': [[(3, 4), '1234']], '1324': [], '1342': [], '1423': [], '1432': []}
+    # Find the linear extension with the least neighbors
+    least_neighbors_count = float('inf')
+    start_node = None
+    for linear_ext in Y_cov:
+        neighbor_count = len(Neighbors.get(linear_ext, []))
+        if neighbor_count < least_neighbors_count:
+            least_neighbors_count = neighbor_count
+            start_node = linear_ext
+
+    print("Start Node:", start_node)
     """
 
-    for a in range(l):
-        G.add_node(upsilon[a])
-        for b in range(a+1, l):
-            pairs = [upsilon[a][i:i+2] for i in range(len(upsilon[a])) if "".join(reversed(upsilon[a][i:i+2])) in upsilon[b] and 
-                     upsilon[a][0:i]+upsilon[a][i+2:len(upsilon[a])] == upsilon[b][0:i]+upsilon[b][i+2:len(upsilon[b])]]
-            if len(pairs) > 0:
-                G.add_edge(upsilon[a], upsilon[b], label=str(sorted([pairs[0][0],pairs[0][1]])))
-                Edges[upsilon[a]].append([tuple(sorted((int(pairs[0][0]), int(pairs[0][1])))), upsilon[b]])
-                Edges[upsilon[b]].append([tuple(sorted((int(pairs[0][0]), int(pairs[0][1])))), upsilon[a]])
+    # Find neighbors of elements in Y_cov that are not yet in Y_cov
+    # Finding mirror
+    tempMirrors = []
+    tempNodes = []
+    for linear_ext in Y_cov:
+        neighbors = Neighbors.get(linear_ext, [])
+        for neighbor in neighbors:
+            if neighbor not in Y_cov:
+                #print(f"Found neighbor {neighbor} of linear extension {linear_ext} not yet in Y_cov")
+                swap = check_swap(linear_ext, neighbor)
+                #print(f"Result of check_swap between {linear_ext} and {neighbor}: {swap}")
+                if swap:
+                    tempMirrors.append((tuple(map(int, swap.split(', '))), [neighbor]))
+                    tempNodes.append(neighbor)
+
+    print("Temp Mirrors:", tempMirrors)
+    print("Temp Nodes:", tempNodes)
+
+    # Finding convex of tempNodes
+    P_tempnodes = generatePoset(tempNodes)
+    print(P_tempnodes)
+    LE_tempnodes = get_linear_extensions(P_tempnodes)
+    print(LE_tempnodes)
     
-    #print("Edges", Edges)
-    while len(G.nodes) > 0:
-        # Create a dictionary of neighbor nodes for each node
-        #neighbors = {n: list(G.neighbors(n)) for n in G.nodes}
-        Neighbors = dict([])
-        for n in list(G.nodes):
-            Neighbors[n]=list(G.neighbors(n))
+    for i in LE_tempnodes:
+        if i in upsilon:
+            Y_cov.append(i)
+            print(Y_cov)
 
-        #print("Neighbors", Neighbors)
-        # Obtain the starting node (node with the least neighbors)
-        numNeighbors = [[len(Neighbors[l]), l] for l in Neighbors]
-        #print("numNeighbors", numNeighbors)
-        numNeighbors = sorted(numNeighbors, key=lambda l: l[0])
-        #print("numNeighbors", numNeighbors)
-        startNode = numNeighbors[0][1]
-        print("startNode:", startNode)
-        # Initialize values for traversal
-        curLE = [startNode]  # List of linear extensions currently in poset
-        print("curLE", curLE)
-        curP = binaryRelation([startNode])  # List of cover relations in current poset
-        print("curP", curP)
-        remEdges = []  # List of edges/anchor pairs to remove from cover relations
+    P_i = generatePoset(Y_cov)
 
-        cond = 1
-        while(cond):
-            # Obtain all edges/anchor pairs connected to members of curLE and their connected nodes
-            potentialPairs = []
-            potentialNodes = dict([])
-            for node in curLE:
-                potentialPairs += [Edges[node][n] for n in range(len(Edges[node])) 
-                                if Edges[node][n][1] in nodes]
-                #print("potentialPairs", potentialPairs)
-                for pair in potentialPairs:
-                    id = tuple(sorted([int(p) for p in pair[0]]))
-                    if potentialNodes.get(id)==None:
-                        potentialNodes[id] = [pair[1]]
-                        #print("potentialNodes[id], None", potentialNodes[id])
-                    elif pair[1] not in potentialNodes[id]:
-                        potentialNodes[id] += [pair[1]]
-                       #print("potentialNodes[id], pair[1] not in potentialNodes[id]", potentialNodes[id])
-                #print("potentialNodes", potentialNodes)
-
-            # Sort potential extensions by frequency from greatest to least 
-            potentialNodes = sorted(potentialNodes.items(), key=lambda x: len(x[1]), reverse=True)
-            #print("potentialNodes", potentialNodes)
-            # Check potential anchor pairs to extend to
-
-            i = 1
-            for potentials in potentialNodes:
-                pairsToRemove = [potentials[0], (potentials[0][1], potentials[0][0])]
-                print("pairsToRemove", pairsToRemove)
-                nodesToAdd = potentials[1]
-                print("nodesToAdd", nodesToAdd)
-                
-                # Debug: Print potential nodes and their connections
-                #print(f"Evaluating potentials: {potentials}")
-                #print(f"Number of nodes connected to the potential anchor pair: {len(potentials[1])}")
-                
-                # If a potential mirror has more than 1 node, include convex of mirror
-                
-                if len(potentials[1]) > 1: # initially 1
-                    mirrors = [set(binaryRelation([x])) for x in potentials[1]]  # list of sets of cover relations of each potential node to add from mirror
-                    nodesToAdd += [s for s in superCover(mirrors, list(G.nodes)) if s not in nodesToAdd]  # list of valid nodes to extend from if convex exists
-                    pairsToRemove += [Edges[l][x][0] for l in nodesToAdd for x in range(len(Edges[l])) if l not in potentials[1]]
-                    pairsToRemove = list(set(pairsToRemove))
-                    print("mirrors", mirrors)
-
-
-                # List of cover relations to potentially add to poset
-                tempNodes = [n for n in list(set(curP + binaryRelation(nodesToAdd))) if n not in remEdges + pairsToRemove and (n[1], n[0]) not in remEdges + pairsToRemove + curP]
-                print("tempNodes", tempNodes)
-
-                # Check if tree poset can be formed and if it will cover the input
-                #P = get_linear_extensions(binaryToCover(tempNodes, len(upsilon[0])))
-                #print("upsilon[0]", upsilon[0])
-                #print("len of upsilon", len(upsilon[0]))
-                #print("binary to cover", binaryToCover(tempNodes, len(upsilon[0])))
-                #P = get_linear_extensions(binaryToCover(tempNodes, len(upsilon[0])))
-
-                P = get_linear_extensions(tempNodes)
-                print("P", P)
-
-                # If the poset is valid, add node/s and continue traversal
-                print("isPoset(tempNodes)", isPoset(tempNodes))
-
-                # Initially, VERIFY is used to check if P and curLE + [p for p in nodesToAdd if p not in curLE] are equal
-                # Now it just checks if the latter is a subset of P
-                print("Verify", VERIFY(P, curLE + [p for p in nodesToAdd if p not in curLE]))
-                print("curLE + nodesToAdd", curLE + [p for p in nodesToAdd if p not in curLE])
-
-                if isPoset(tempNodes) and VERIFY(P, curLE + [p for p in nodesToAdd if p not in curLE]):
-                    curP = tempNodes
-                    curLE += [p for p in nodesToAdd if p not in curLE]
-                    nodes = [n for n in nodes if n not in curLE]
-                    remEdges += pairsToRemove
-                    print("curP", curP)
-                    print("curLE", curLE)
-                    print("nodes", nodes)
-                    print("remEdges", remEdges)
-                    break
-                # If none of the potential node/s yield a valid poset, end traversal and continue with remaining graph
-                elif i >= len(potentialNodes):
-                    cond = 0
-                    #print("potentialNodes", potentialNodes)
-                i += 1
-
-            # If there are no potential nodes to extend to, end traversal and continue with remaining graph
-            if len(potentialNodes) == 0:
-                cond = 0
-
-
-        """
-        # For drawing the transposition graphs
-        pos = nx.spring_layout(G)
-        nx.draw_networkx(G, pos, with_labels=True,font_size=10, node_size=500, node_color='#9CE5FF')
-        nx.draw_networkx_edge_labels(G, pos, edge_labels=nx.get_edge_attributes(G,'label'))
-
-        p.show()
-        """
-
-        nodes = [n for n in nodes if n not in curLE]
-        
-        # Remove nodes added to a tree poset from graph
-        for le in curLE:
-            G.remove_node(le)
-
-        # Add poset to poset tree, start over with remaining graph
-        Pset.append(curP)
-    
-    # Return list of posets found
-    return Pset
-
+    return P_i
 
 def main():
-    
     sample_input = [
         '14523', '14253', '14235', '12453', '12435', '12345', '13245', '13425',
-                '12354', '12534', '13254', '13524'
+        '12354', '12534', '13254', '13524'
     ]
     """
     sample_input = [
@@ -183,26 +89,19 @@ def main():
         '12453', '12345', '13425', '13524', '12354', '12534', '12435', '14523', '14235', '13254', '13245', '14253'
     ]
     """
+    """
+    
+
     P_A = [(1, 2), (1, 3), (1, 4), (1, 5), (2, 4), (3, 2), (3, 4), (3, 5), (5, 2), (5, 4)]
     A = ((5, 2), (2, 4))
+    """
+    P_A = [(1, 2), (1, 3), (1, 4), (1, 5), (2, 3), (4, 2), (4, 3), (4, 5)]
     
-    output = Poset(sample_input)
+    A = ((4, 2), (4, 3))
+    
+    output = maximalPoset(sample_input, P_A, A)
     for poset in output:
         print(poset)
-    # count = 1
-    # for inputLinearOrders in sample_input:
-    #     input_list = [int(x) for x in str(inputLinearOrders)]
-    #     print(f"Input: {input_list}")
-    #     posets = Poset(input_list)
-    #     posets = poset.generateLinearExtensions()
-    #     if posets:
-    #         for i, linear_order in enumerate(posets):
-    #             print(f"P{i+1}: {linear_order}")
-    #     else:
-    #         print(f"No posets found for input: {input_list}")
-    #     print()
 
 if __name__ == "__main__":
     main()
-
-
--- Algorithm/1updated/algo2_1.py
+++ Algorithm/1updated/algo2_1.py
@@ -6,7 +6,6 @@ from poset_utils import VERIFY, get_linear_extensions, binaryRelation, binaryToC
 from algo1 import generatePoset
 from permutohedron import check_swap
 
-
 def maximalPoset(upsilon, P_A, A):
     G = nx.Graph()
     N = len(upsilon)
@@ -14,27 +13,24 @@ def maximalPoset(upsilon, P_A, A):
     # Add an edge between each node that has an adjacent swap
     for i in range(N):
         G.add_node(upsilon[i])
-        for j in range(i+1, N):
+        for j in range(i + 1, N):
             adjacent = check_swap(upsilon[i], upsilon[j])
             if adjacent:
                 G.add_edge(upsilon[i], upsilon[j], label=adjacent, color='k')
 
     # Find linear extensions
     Y_cov = get_linear_extensions(P_A)
-    print("Y_A", Y_cov)
+    print("Y_cov", Y_cov)
     Y_uncov = [u for u in upsilon if u not in Y_cov]
     print("Y_uncov", Y_uncov)
 
-
     # Create a dictionary of neighbor nodes for each node
-    #neighbors = {n: list(G.neighbors(n)) for n in G.nodes}
-    Neighbors = dict([])
-    for n in list(G.nodes):
-        Neighbors[n]=list(G.neighbors(n))
+    Neighbors = {n: list(G.neighbors(n)) for n in G.nodes()}
 
-    print("Neighbors", Neighbors)
+    #print("Neighbors", Neighbors)
 
-    #Check least amount of neighbors in Y_cov
+    """
+    # Find the linear extension with the least neighbors
     least_neighbors_count = float('inf')
     start_node = None
     for linear_ext in Y_cov:
@@ -44,11 +40,40 @@ def maximalPoset(upsilon, P_A, A):
             start_node = linear_ext
 
     print("Start Node:", start_node)
+    """
 
-    
-    
+    # Find neighbors of elements in Y_cov that are not yet in Y_cov
+    # Finding mirror
+    tempMirrors = []
+    tempNodes = []
+    for linear_ext in Y_cov:
+        neighbors = Neighbors.get(linear_ext, [])
+        for neighbor in neighbors:
+            if neighbor not in Y_cov:
+                #print(f"Found neighbor {neighbor} of linear extension {linear_ext} not yet in Y_cov")
+                swap = check_swap(linear_ext, neighbor)
+                #print(f"Result of check_swap between {linear_ext} and {neighbor}: {swap}")
+                if swap:
+                    tempMirrors.append((tuple(map(int, swap.split(', '))), [neighbor]))
+                    tempNodes.append(neighbor)
 
-    return Y_cov
+    print("Temp Mirrors:", tempMirrors)
+    print("Temp Nodes:", tempNodes)
+
+    # Finding convex of tempNodes
+    P_tempnodes = generatePoset(tempNodes)
+    print(P_tempnodes)
+    LE_tempnodes = get_linear_extensions(P_tempnodes)
+    print(LE_tempnodes)
+    
+    for i in LE_tempnodes:
+        if i in upsilon:
+            Y_cov.append(i)
+            print(Y_cov)
+
+    P_i = generatePoset(Y_cov)
+
+    return P_i
 
 def main():
     sample_input = [
@@ -64,8 +89,14 @@ def main():
         '12453', '12345', '13425', '13524', '12354', '12534', '12435', '14523', '14235', '13254', '13245', '14253'
     ]
     """
-    #P_A = [(1, 2), (1, 3), (1, 4), (1, 5), (2, 4), (3, 2), (3, 4), (3, 5), (5, 2), (5, 4)] # ((5, 2), (2, 4))
-    P_A = [(1, 2), (1, 3), (1, 4), (1, 5), (2, 3), (4, 2), (4, 3), (4, 5)] # ((4, 2), (4, 3))
+    """
+    
+
+    P_A = [(1, 2), (1, 3), (1, 4), (1, 5), (2, 4), (3, 2), (3, 4), (3, 5), (5, 2), (5, 4)]
+    A = ((5, 2), (2, 4))
+    """
+    P_A = [(1, 2), (1, 3), (1, 4), (1, 5), (2, 3), (4, 2), (4, 3), (4, 5)]
+    
     A = ((4, 2), (4, 3))
     
     output = maximalPoset(sample_input, P_A, A)
