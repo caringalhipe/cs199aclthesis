41684975c023d6de632164283bee31ba04da7054
Em Caringal <91007625+ircaringal@users.noreply.github.com>
2024-06-08 15:59:31 +0800

    maximal poset

--- Algorithm/1updated/algo2_1.py
+++ Algorithm/1updated/algo2_1.py
@@ -20,9 +20,9 @@ def maximalPoset(upsilon, P_A, A):
 
     # Find linear extensions
     Y_cov = get_linear_extensions(P_A)
-    print("Y_cov", Y_cov)
+    #print("Y_cov", Y_cov)
     Y_uncov = [u for u in upsilon if u not in Y_cov]
-    print("Y_uncov", Y_uncov)
+    #print("Y_uncov", Y_uncov)
 
     # Create a dictionary of neighbor nodes for each node
     Neighbors = {n: list(G.neighbors(n)) for n in G.nodes()}
@@ -41,35 +41,53 @@ def maximalPoset(upsilon, P_A, A):
 
     print("Start Node:", start_node)
     """
+    while(1):
+        # Find neighbors of elements in Y_cov that are not yet in Y_cov
+        # Finding mirror
+        tempMirrors = []
+        tempNodes = []
+        for linear_ext in Y_cov:
+            neighbors = Neighbors.get(linear_ext, [])
+            for neighbor in neighbors:
+                if neighbor not in Y_cov:
+                    #print(f"Found neighbor {neighbor} of linear extension {linear_ext} not yet in Y_cov")
+                    swap = check_swap(linear_ext, neighbor)
+                    #print(f"Result of check_swap between {linear_ext} and {neighbor}: {swap}")
+                    if swap:
+                        tempMirrors.append((tuple(map(int, swap.split(', '))), [neighbor]))
+                        tempNodes.append(neighbor)
+        
+        if len(tempNodes) == 0:
+            break
 
-    # Find neighbors of elements in Y_cov that are not yet in Y_cov
-    # Finding mirror
-    tempMirrors = []
-    tempNodes = []
-    for linear_ext in Y_cov:
-        neighbors = Neighbors.get(linear_ext, [])
-        for neighbor in neighbors:
-            if neighbor not in Y_cov:
-                #print(f"Found neighbor {neighbor} of linear extension {linear_ext} not yet in Y_cov")
-                swap = check_swap(linear_ext, neighbor)
-                #print(f"Result of check_swap between {linear_ext} and {neighbor}: {swap}")
-                if swap:
-                    tempMirrors.append((tuple(map(int, swap.split(', '))), [neighbor]))
-                    tempNodes.append(neighbor)
+        print("Temp Mirrors:", tempMirrors)
+        print("Temp Nodes:", tempNodes)
 
-    print("Temp Mirrors:", tempMirrors)
-    print("Temp Nodes:", tempNodes)
-
-    # Finding convex of tempNodes
-    P_tempnodes = generatePoset(tempNodes)
-    print(P_tempnodes)
-    LE_tempnodes = get_linear_extensions(P_tempnodes)
-    print(LE_tempnodes)
+        # Finding convex of tempNodes
+        P_tempnodes = generatePoset(tempNodes)
+        print(f"P_tempnodes: {P_tempnodes}")
+        LE_tempnodes = get_linear_extensions(P_tempnodes)
+        print(f"LE_tempnodes: {LE_tempnodes}")
+        print()
+        
+        temp_cov = []
+        for i in LE_tempnodes:
+            if i in upsilon:
+                temp_cov.append(i)
+        print(f"Y_cov: {Y_cov}")
+        print(f"Y_cov, if extended: {list(set(Y_cov).union(set(temp_cov)))}")
+        if sorted(get_linear_extensions(generatePoset(list(set(Y_cov).union(set(temp_cov)))))) == sorted(list(set(Y_cov).union(set(temp_cov)))):
+            Y_cov = list(set(Y_cov).union(set(temp_cov)))
+            print(f"Y_cov, after extending: {sorted(Y_cov)}\n")
+        else:
+            print(f"Poset of extended Y_cov: {generatePoset(list(set(Y_cov).union(set(temp_cov))))}")
+            print(f"Linear orders of extended Y_cov do not match extended Y_cov:")
+            print(f"Extended Y_cov: {sorted(list(set(Y_cov).union(set(temp_cov))))}")
+            print(F"LEs of extended Y_cov: {sorted(get_linear_extensions(generatePoset(list(set(Y_cov).union(set(temp_cov))))))}")
+            print("IZ WRONG!!!!!!!!!!!!!!!!!\n")
+            break
     
-    for i in LE_tempnodes:
-        if i in upsilon:
-            Y_cov.append(i)
-            print(Y_cov)
+    print(f"Y_cov: {Y_cov}")
 
     P_i = generatePoset(Y_cov)
 
@@ -95,9 +113,9 @@ def main():
     P_A = [(1, 2), (1, 3), (1, 4), (1, 5), (2, 4), (3, 2), (3, 4), (3, 5), (5, 2), (5, 4)]
     A = ((5, 2), (2, 4))
     """
-    P_A = [(1, 2), (1, 3), (1, 4), (1, 5), (2, 3), (4, 2), (4, 3), (4, 5)]
+    P_A = [(1, 2), (1, 3), (1, 4), (1, 5), (3, 5), (3, 2), (3, 4), (5, 2), (5, 4), (2, 4)]
     
-    A = ((4, 2), (4, 3))
+    A = ((5, 2), (2, 4))
     
     output = maximalPoset(sample_input, P_A, A)
     for poset in output:
